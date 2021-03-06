2c6de8c43ce5b51eb54d2df16a48cc7c454a413b
subham <berasubham8@gmail.com>
2021-03-07 02:34:10 +0530

    404-feature

--- index.js
+++ index.js
@@ -5,6 +5,17 @@ http
         console.log(res);
         res.write("<h1>Home Page</h1>");
         res.end();
-
-    })
+    });
+	response.write("<h1>Home</h1>");
+	response.end();
+	break;
+	default:
+		response.writeHead(404,{
+			"Content-Type":"text/html",
+    	});
+	response.write("<h1>Home</h1>");
+	response.end();
+	break;
+}
+})
     .listen(8080);
