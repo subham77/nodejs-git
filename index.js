f8ec1eef5e53915dc889cf5bb99c9e4239a736c6
subham <berasubham8@gmail.com>
2021-03-07 15:47:52 +0530

    about-feature

--- index.js
+++ index.js
@@ -1,10 +1,22 @@
 const http=require("https");
 
 http
-        .createServer((req,res)=> {
-        console.log(res);
-        res.write("<h1>Home Page</h1>");
-        res.end();
-
-    })
-    .listen(8080);
+	.createServer((req,res)=> {
+		switch(request.url){
+			case "/":
+				response.writeHead(200,{
+					"Content-Type":"text/html",
+				});
+				response.write("<h1>Home Page!</h1>");
+				response.end();
+				break;
+			case "/about":
+				response.writeHead(200,{
+				"Content-Type":"text/html",
+				});
+				response.write("<h2>Home Page!</h2>");
+				response.end();
+				break;
+			}
+   	 })
+    	.listen(8080);
